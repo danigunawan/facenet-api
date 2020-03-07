eb9851e11a5993ce1f7f6082c076f98fa666011c
Harsh Mahajan <harshmahajan927@yahoo.com>
2020-03-07 12:28:44 +0530

    Add pre process to train route

--- api/routes/train-route.js
+++ api/routes/train-route.js
@@ -5,41 +5,60 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   console.log("Train model requested");
 
-  exec("cd core && python3 train_main.py", function(error, stdout, stderr) {
-    console.log(stdout);
-    var responseList = stdout.split(/\r?\n/);
+  console.log("Attempting to pre process images");
 
-    var classes = -1,
-      totalImages = -1,
-      successful = false;
-    responseList.forEach(function(consoleLine) {
-      if (consoleLine.includes("Classes")) {
-        classes = parseInt(consoleLine.replace("Classes: ", ""));
+  exec(
+    "rm core/pre_img/bounding* && cd core && python3 data_preprocess.py",
+    function(error, stdout, stderr) {
+      console.log(stdout);
+      if (stdout.includes("Completed")) {
+        exec("cd core && python3 train_main.py", function(
+          error,
+          stdout,
+          stderr
+        ) {
+          console.log(stdout);
+          var responseList = stdout.split(/\r?\n/);
+
+          var classes = -1,
+            totalImages = -1,
+            successful = false;
+          responseList.forEach(function(consoleLine) {
+            if (consoleLine.includes("Classes")) {
+              classes = parseInt(consoleLine.replace("Classes: ", ""));
+            }
+
+            if (consoleLine.includes("Images")) {
+              totalImages = parseInt(consoleLine.replace("Images: ", ""));
+            }
+
+            if (consoleLine.includes("Completed")) {
+              successful = true;
+            }
+          });
+
+          if (successful) {
+            return res.status(200).json({
+              statusCode: 200,
+              statusMessage: "Model re-trained successfully",
+              classes: classes,
+              totalImages: totalImages
+            });
+          } else {
+            return res.status(400).json({
+              statusCode: 400,
+              statusMessage: "Model training failed"
+            });
+          }
+        });
+      } else {
+        return res.status(400).json({
+          statusCode: 400,
+          statusMessage: "Model training failed"
+        });
       }
-
-      if (consoleLine.includes("Images")) {
-        totalImages = parseInt(consoleLine.replace("Images: ", ""));
-      }
-
-      if (consoleLine.includes("Completed")) {
-        successful = true;
-      }
-    });
-
-    if (successful) {
-      return res.status(200).json({
-        statusCode: 200,
-        statusMessage: "Model re-trained successfully",
-        classes: classes,
-        totalImages: totalImages
-      });
-    } else {
-      return res.status(404).json({
-        statusCode: 404,
-        statusMessage: "Model training failed"
-      });
     }
-  });
+  );
 });
 
 module.exports = router;
--- core/data_preprocess.py
+++ core/data_preprocess.py
@@ -8,3 +8,4 @@ nrof_images_total, nrof_successfully_aligned = obj.collect_data()
 
 print('Total number of images: %d' % nrof_images_total)
 print('Number of successfully aligned images: %d' % nrof_successfully_aligned)
+print('Completed')
